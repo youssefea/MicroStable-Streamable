71be81dc463af11eb375883cc69ecac72c4b3c36
shafu <shafu0x@gmail.com>
2024-11-23 19:34:13 +0100

    fix

--- src/MicroStable.sol
+++ src/MicroStable.sol
@@ -67,9 +67,9 @@ contract Manager {
   }
 
   function collatRatio(address user) public view returns (uint) {
-    uint mintedShusd = address2minted[user];
-    if (mintedShusd == 0) return type(uint256).max;
+    uint minted = address2minted[user];
+    if (minted == 0) return type(uint256).max;
     uint totalValue = address2deposit[user] * oracle.latestAnswer() / 1e18;
-    return totalValue / mintedShusd;
+    return totalValue / minted;
   }
 }
