07e269582aade562f95048fa987939b74e56aa95
shafu <shafu0x@gmail.com>
2024-11-23 10:01:11 +0100

    liquidate

--- src/MicroStable.sol
+++ src/MicroStable.sol
@@ -20,8 +20,10 @@ contract ShUSD is ERC20("Shafu USD", "shUSD", 18) {
 }
 
 contract Manager {
-  ERC20  public weth;
-  ShUSD  public shUSD;
+  uint public constant MIN_COLLAT_RATIO = 1.5e18;
+
+  ERC20 public weth;
+  ShUSD public shUSD;
 
   Oracle public oracle;
 
@@ -39,10 +41,35 @@ contract Manager {
     address2deposit[msg.sender] += amount;
   }
 
-  function collatRatio() public view returns (uint) {
-    uint mintedDyad = address2minted[msg.sender];
+  function withdraw(uint amount) public {
+    address2deposit[msg.sender] -= amount;
+    require(collatRatio(msg.sender) >= MIN_COLLAT_RATIO);
+    weth.transfer(msg.sender, amount);
+  }
+
+  function burn(uint amount) public {
+    address2minted[msg.sender] -= amount;
+    shUSD.burn(msg.sender, amount);
+  }
+
+  function mint(uint amount) public {
+    address2minted[msg.sender] += amount;
+    require(collatRatio(msg.sender) >= MIN_COLLAT_RATIO);
+    shUSD.mint(msg.sender, amount);
+  }
+
+  function liquidate(address user) public {
+    require(collatRatio(user) < MIN_COLLAT_RATIO);
+    shUSD.burn(user, address2minted[user]);
+    weth.transfer(user, address2deposit[user]);
+    address2deposit[user] = 0;
+    address2minted[user] = 0;
+  }
+
+  function collatRatio(address user) public view returns (uint) {
+    uint mintedDyad = address2minted[user];
     if (mintedDyad == 0) return type(uint256).max;
-    uint totalValue = address2deposit[msg.sender] * oracle.latestAnswer() / 1e18;
+    uint totalValue = address2deposit[user] * oracle.latestAnswer() / 1e18;
     return totalValue / mintedDyad;
   }
 }
